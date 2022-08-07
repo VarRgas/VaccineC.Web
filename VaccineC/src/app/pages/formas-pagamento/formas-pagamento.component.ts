0986c72f15cf5d906b1700ab2525b743326fb969
VarRgas <89429606+VarRgas@users.noreply.github.com>
2022-08-07 15:28:37 -0300

    PaymentMethod

--- VaccineC/src/app/pages/formas-pagamento/formas-pagamento.component.ts
+++ VaccineC/src/app/pages/formas-pagamento/formas-pagamento.component.ts
@@ -1,11 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PeriodicElement {
+  paymentForm: string;
+  parcels: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { paymentForm: 'Boleto', parcels: 1},
+  { paymentForm: 'Cartão de crédito', parcels: 5},
+  { paymentForm: 'Cartão de débito', parcels: 1},
+  { paymentForm: 'Cheque', parcels: 20}
+];
+
 @Component({
   selector: 'app-formas-pagamento',
   templateUrl: './formas-pagamento.component.html',
   styleUrls: ['./formas-pagamento.component.scss']
 })
 export class FormasPagamentoComponent implements OnInit {
+  value = '';
+  value2 = '';
+
+  displayedColumns: string[] = ['paymentForm', 'parcels'];
+  dataSource = ELEMENT_DATA;
 
   constructor() { }
 
