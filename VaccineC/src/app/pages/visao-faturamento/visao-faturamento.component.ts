51f3e499f318e4fe8a0e3590eca5a6af9dc3dbd5
VarRgas <89429606+VarRgas@users.noreply.github.com>
2022-08-09 08:48:07 -0300

    Visão de faturamento (inconcluido)

--- VaccineC/src/app/pages/visao-faturamento/visao-faturamento.component.ts
+++ VaccineC/src/app/pages/visao-faturamento/visao-faturamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-visao-faturamento',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./visao-faturamento.component.scss']
 })
 export class VisaoFaturamentoComponent implements OnInit {
+  longText = `Exemplo`;
+  currentDate = '01/05/2022';
+  endDate = '20/05/2022';
+  invoicing = 'R$ 7.130,00';
+  profitEstimate = 'R$ 7.870,00'
 
   constructor() { }
 
