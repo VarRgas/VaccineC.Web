5c6eff7e1f6ac4e24dd00fb85586011667d252ec
Amanda Maschio <maschioamanda@hotmail.com>
2022-08-18 22:01:56 -0300

    Login

--- VaccineC/src/app/pages/login/login.component.ts
+++ VaccineC/src/app/pages/login/login.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LoginModel } from 'src/app/models/login-model';
 import { LoginDispatcherService } from 'src/app/services/login-dispatcher.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,18 +12,20 @@ import { LoginDispatcherService } from 'src/app/services/login-dispatcher.servic
 })
 export class LoginComponent implements OnInit {
 
-  public exampleInputEmail!: string; 
-  public exampleInputPassword!: string; 
-
+  public userEmail!: string;
+  public userPassword!: string;
+  
   loginForm: FormGroup = this.formBuilder.group({
     email: [null, [Validators.required]],
     password: [null, [Validators.required]]
   });
 
-  constructor(private loginDispatcherService: LoginDispatcherService,
-    private formBuilder: FormBuilder,) {
-
-  }
+  constructor(
+    private loginDispatcherService: LoginDispatcherService,
+    private formBuilder: FormBuilder,
+    private snackBar: MatSnackBar,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
   }
@@ -29,10 +33,26 @@ export class LoginComponent implements OnInit {
   public validateLogin(): void {
 
     let login = new LoginModel();
-    login.email = this.exampleInputEmail;
-    login.password = this.exampleInputPassword;
+    login.email = this.userEmail;
+    login.password = this.userPassword;
 
-    this.loginDispatcherService.Login(login).subscribe(a => {
-    })
+    this.loginDispatcherService.Login(login).subscribe(
+      response => {
+        console.log(response);
+        this.snackBar.open("Logado com sucesso!", 'Ok', {
+          duration: 5000
+        });
+        this.router.navigate(['/', 'home']);
+      },
+      error => {
+        console.log(error);
+        this.snackBar.open('Usuário ou senha inválidos, verifique e tente novamente!', 'Ok', {
+          horizontalPosition: 'center',
+          verticalPosition: 'top',
+          duration: 5000
+        });
+        this.userEmail = '';
+        this.userPassword = '';
+      });
   }
 }
