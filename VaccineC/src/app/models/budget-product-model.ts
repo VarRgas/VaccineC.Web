198da739760343bc72ea599bdc483d9092729d6e
Amanda Maschio <maschioamanda@hotmail.com>
2022-09-26 10:04:32 -0300

    BudgetProduct

---
+++ VaccineC/src/app/models/budget-product-model.ts
@@ -0,0 +1,11 @@
+export class BudgetProductModel {
+    public id!: string;
+    public budgetId!: string;
+    public productId!: string;
+    public borrowerPersonId!: string;
+    public productDose!: string;
+    public details!: string;
+    public estimatedSalesValue!: number;
+    public situationProduct!: string;
+    public register!: Date;
+}
--- VaccineC/src/app/services/budgets-products-dispatcher.service.ts
+++ VaccineC/src/app/services/budgets-products-dispatcher.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { BudgetProductModel } from '../models/budget-product-model';
 
 const baseURL = 'http://localhost:5000/api/BudgetsProducts';
 
@@ -22,6 +23,22 @@ export class BudgetsProductsDispatcherService {
 
     public getBudgetsProductsBudget(budgetId: string): Observable<any> {
         return this.httpClient.get(`${baseURL}/${budgetId}/GetBudgetsProductsByBudget`);
+    }
+
+    public createBudgetProduct(data: object): Observable<any> {
+        return this.httpClient.post(`${baseURL}/Create`, data);
+    }
+
+    public createOnDemand(data: BudgetProductModel[]): Observable<any> {
+        return this.httpClient.post(`${baseURL}/CreateOnDemand`, data);
+      }
+
+    public updateBudgetProduct(id: string, data: object): Observable<any> {
+        return this.httpClient.put(`${baseURL}/${id}/Update`, data);
+      }
+
+    public deleteBudgetProduct(id: string): Observable<any> {
+        return this.httpClient.delete(`${baseURL}/${id}/Delete`);
     }
 
 }
